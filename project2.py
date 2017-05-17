38066ce56296af35343ab2c1067500d115537cc0
lightinglark <ryancpalm@gmail.com>
2017-05-17 10:16:36 -0700

    first commit

---
+++ project2.py
@@ -0,0 +1,29 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Created on May 17, 2017
+
+Project 2: (Project 2 ADT definition)
+
+Dependencies: scikit-learn
+
+Assumptions and Implementation Notes:
+            -- 
+
+
+@authors: Camilo Acosta, Jayse Farrel, Jessica Kunder, Ryan Palm
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+__author__ = "Camilo Acosta, Jayse Farrel, Jessica Kunder, Ryan Palm"
+__copyright__ = "COPYRIGHT_INFORMATION"
+__credits__ = ["Camilo Acosta, Jayse Farrel, Jessica Kunder, Ryan Palm"]
+__license__ = "GPL"
+__version__ = "1.6.0dev"
+__maintainer__ = "AUTHOR_NAME"
+__email__ = "AUTHOR_EMAIL"
+__status__ = "homework"
+
+def main():
+    
+    print("More to come.")
+    
+
+main()
